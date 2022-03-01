c075c2722718fe3a4a694bf774fbd6c215dcff2f
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2022-03-01 09:46:17 +0100

    Create delete.ps1

---
+++ delete.ps1
@@ -0,0 +1,109 @@
+#####################################################
+# HelloID-Conn-Prov-Target-SpeakApp-Delete
+#
+# Version: 1.0.0
+#####################################################
+# Initialize default value's
+$config = $configuration | ConvertFrom-Json
+$p = $person | ConvertFrom-Json
+$aRef = $AccountReference | ConvertFrom-Json
+$success = $false
+$auditLogs = [System.Collections.Generic.List[PSCustomObject]]::new()
+
+# Account mapping
+$account = [ordered]@{
+    schemas  = @("urn:ietf:params:scim:schemas:core:2.0:User")
+    userName = $p.ExternalId
+    active   = $false
+}
+
+# Enable TLS1.2
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+# Set debug logging
+switch ($($config.IsDebug)) {
+    $true { $VerbosePreference = 'Continue' }
+    $false { $VerbosePreference = 'SilentlyContinue' }
+}
+
+#region functions
+function Resolve-HTTPError {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,
+            ValueFromPipeline
+        )]
+        [object]$ErrorObject
+    )
+    process {
+        $httpErrorObj = [PSCustomObject]@{
+            FullyQualifiedErrorId = $ErrorObject.FullyQualifiedErrorId
+            MyCommand             = $ErrorObject.InvocationInfo.MyCommand
+            RequestUri            = $ErrorObject.TargetObject.RequestUri
+            ScriptStackTrace      = $ErrorObject.ScriptStackTrace
+            ErrorMessage          = ''
+        }
+        if ($ErrorObject.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') {
+            $httpErrorObj.ErrorMessage = $ErrorObject.ErrorDetails.Message
+        } elseif ($ErrorObject.Exception.GetType().FullName -eq 'System.Net.WebException') {
+            $httpErrorObj.ErrorMessage = [System.IO.StreamReader]::new($ErrorObject.Exception.Response.GetResponseStream()).ReadToEnd()
+        }
+        Write-Output $httpErrorObj
+    }
+}
+#endregion
+
+try {
+    Write-Verbose "Deleting SpeakApp account for: '[$($p.DisplayName)]'"
+    Write-Verbose 'Adding token to authorization headers'
+    $headers = [System.Collections.Generic.Dictionary[string, string]]::new()
+    $headers.Add("Authorization", "Bearer $($config.ApiToken)")
+
+    # Add an auditMessage showing what will happen during enforcement
+    if ($dryRun -eq $true){
+        $auditLogs.Add([PSCustomObject]@{
+            Message = "Delete SpeakApp account from: [$($p.DisplayName)], will be executed during enforcement"
+        })
+    }
+
+    if (-not($dryRun -eq $true)) {
+        Write-Verbose "Deleting SpeakApp account: [$aRef] from: [$($p.DisplayName)]"
+
+        $splatParams = @{
+            Uri         = "$($config.BaseUrl)/Users/$aRef"
+            Headers     = $headers
+            Body        = $account | ConvertTo-Json -Depth 20
+            Method      = 'PUT'
+            ContentType = 'application/scim+json'
+        }
+        $results = Invoke-RestMethod @splatParams -Verbose:$false
+        if ($results.id){
+            $success = $true
+            $auditLogs.Add([PSCustomObject]@{
+                Message = "Delete account for: $($p.DisplayName) was successful."
+                IsError = $False
+            })
+        }
+    }
+} catch {
+    $success = $false
+    $ex = $PSItem
+    if ($($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or
+        $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
+        $errorObj = Resolve-HTTPError -ErrorObject $ex
+        $errorMessage = "Could not delete SpeakApp account for: [$($p.DisplayName)]. Error: $($errorObj.ErrorMessage)"
+    } else {
+        $errorMessage = "Could not delete SpeakApp account for: [$($p.DisplayName)]. Error: $($ex.Exception.Message)"
+    }
+    Write-Verbose $errorMessage
+    $auditLogs.Add([PSCustomObject]@{
+        Message = $errorMessage
+        IsError = $true
+    })
+} finally {
+    $result = [PSCustomObject]@{
+        Success   = $success
+        Auditlogs = $auditLogs
+    }
+    Write-Output $result | ConvertTo-Json -Depth 10
+}
